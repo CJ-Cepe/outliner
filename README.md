2360fe567219551f67b57d61aee18c503f820780
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2024-03-28 18:27:05 +0800

    Refactor: add/remove outline to all queried tabs

--- README.md
+++ README.md
@@ -14,9 +14,16 @@
             - setOutline(data.sliderState)
             - if not, nothing happens since it is OFF by default
         - useEffect to monitor changes and save current state to local storage
+    - [] add/remove style when slider is toggled
+        - send message to background
 - [] add outline style to all
-    - [] inject style 
-        - chrome scripting in the background.js
+    - use chrome.tabs.query to query tab
+    - use chrome.scripting.executeScript to inject script
+    - when sliderState is true
+        - inject style
+    - when sliderState is false
+        - remove style
+    
         
 - [] add outline style to all
     - [] What if the page reloads while turned ON
@@ -24,8 +31,13 @@
     - [] all tabs or current tab only?
     - [] what if turned on then close browser then open again
         - [] what if crashes and got turned off
+    - [] when turned ON and tab is loaded
 
-
+- PERMISSIONS
+    - tabs -> for querying tabs
+    - scripting -> for appending/removing outline style
+    - storage -> for saving button and outline attrib 
+    - <all_urls> -> to add outline to all pages
 
 
 # Cases
--- public/background.js
+++ public/background.js
@@ -1,117 +1,51 @@
-console.log('background')
 
+//Receive message from popup
+chrome.runtime.onMessage.addListener(handleMessage);
 
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    const {action, sliderState, outline} = message
+async function handleMessage(message, sender, sendResponse) {
+  const {action, sliderState, outline} = message
 
-    if (action === 'toggleSlider') {
-      if (sliderState) {
-        console.log('State ON')
-        getTab();
-        /* injectStyle() */
-        //add style if it exist
-      } 
-      
-      //slider False
-      else {
-        console.log('State OFF')
-        removeTab();
-        //remove style if it exist
-      }
-    }
-});
+  if (action === 'toggleSlider') {
+    const tabs = await getTabs()
 
-const urls = "*://*/*"
-
-  function getTab(){
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      if (tabs.length > 0) {
-        const tabId = tabs[0].id;
-        chrome.scripting.executeScript({
-          target: { tabId },
-          func: injectStyle,
-        }).then(() => console.log("injected a function"));
-      }
-    });
-
-    function injectStyle(){
-      styleElement = document.createElement('link');
-      styleElement.href = chrome.runtime.getURL('outline.css')
-      styleElement.rel = 'stylesheet';
-      styleElement.type = 'text/css'
-      styleElement.dataset.ext = "outlineExtStyle"
-      document.head.appendChild(styleElement);
-    }
-    
-  }
-
-  function removeTab(){
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      if (tabs.length > 0) {
-        const tabId = tabs[0].id;
-        chrome.scripting.executeScript({
-          target: { tabId },
-          func: removeStyle,
-        }).then(() => console.log("injected a function"));
-      }
-    });
-
-    function removeStyle(){
-      const element = document.head.querySelector('link[data-ext = outlineExtStyle]')
-      document.head.removeChild(element)
+    //add style
+    if(sliderState){
+      tabs.forEach((tab)=>{
+        toggleStyle(tab, addStyle)
+      })
     } 
-  }
- 
-
-
-
-/* function createStyle(){
-    const style = " * { outline: 1px solid red } body { background-color: red }"
-    const styleElement = document.createElement('style')
-    styleElement.textContent = style;
-    document.head.appendChild(styleElement)
-  } */
-
-  /*     const response = await fetch(chrome.runtime.getURL('outline.css'));
-    const cssText = response.text() */
-
-  /* function createStyle(){
-    console.log('yeah!!!')
-    styleElement = document.createElement('link');
-    styleElement.href = chrome.runtime.getURL('outline.css') // Replace with your stylesheet path
-    styleElement.rel = 'stylesheet';
-    document.head.appendChild(styleElement);
-  } */
-/* function injectStyle(){
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    if (tabs.length > 0) {
-      const tabId = tabs[0].id;
-      chrome.scripting.executeScript({target: {tabId}, files: ['outline.css'] });
+    //remove style
+    else {
+      tabs.forEach((tab)=>{
+        toggleStyle(tab, removeStyle)
+      })
     }
-  })
-} */
-
-
-
-
-/* 
-//OLD
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.tabs.query({ url: urls }, (tabs) => {
-    for (const tab of tabs) {
-      chrome.tabs.executeScript(tab.id, { file: "content.js" });
-    }
-  });
-});
-
-
-//NEW
-function injectStyles() {
-  chrome.tabs.query({ url: url }, (tabs) => { // Matches all web pages
-  for (const tab of tabs) {
-    chrome.tabs.sendMessage(tab.id, { action: 'injectStyles' });
   }
-});
 }
 
- */
+async function getTabs(){
+  const tabs = await chrome.tabs.query({})
+  return tabs
+}
+
+function toggleStyle(tab, func){
+  const tabId = tab.id
+  chrome.scripting.executeScript({
+    target: {tabId},
+    func: func,
+  })
+}
+
+function addStyle(){
+  styleElement = document.createElement('link');
+  styleElement.href = chrome.runtime.getURL('outline.css')
+  styleElement.rel = 'stylesheet';
+  styleElement.type = 'text/css'
+  styleElement.dataset.ext = "outlineExtStyle"
+  document.head.appendChild(styleElement);
+}
+
+function removeStyle(){
+  const element = document.head.querySelector('link[data-ext = outlineExtStyle]')
+  document.head.removeChild(element)
+}
